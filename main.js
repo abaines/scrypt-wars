8a209a9be6230541a68f1decd6dc9353e114ac7a
Alan Baines <abaines.vt@gmail.com>
2018-03-31 09:38:40 -0400

    dynamic units

--- main.js
+++ main.js
@@ -2,8 +2,26 @@
 
 console.log("hello!");
 
+const tileSize = 64;
+
 $.getJSON( "map1/units", function( data ) {
-	  console.log(data);
+	console.log(data);
+
+	const xoffset = 8;
+	const yoffset = 164;
+
+	var unitId = 0;
+	data.forEach(function(unit) {
+		var x = xoffset + tileSize*unit.location[0];
+		var y = yoffset + tileSize*unit.location[1];
+		
+		console.log(unit.img, x, y, unit.location);
+
+		$( "#map" ).append( '<img id="unit'+unitId+'" src="'+unit.img+'" style="position:absolute;width:64px;height:64px;"/>' );
+		$( "#unit"+unitId ).css("left",x).css("top",y);
+		
+		unitId++;
+	});
 });
 
 $.getJSON( "map1/terrain", function( data ) {
@@ -11,13 +29,10 @@ $.getJSON( "map1/terrain", function( data ) {
 
 	var r = 0;
 	data.forEach(function(row) {
-  		console.log(row);
-  		
+
   		var c = 0;
   		row.forEach(function(el)
   		{
-  			console.log(r,c,el);
-  			
   			if (el=="P")
   			{
   				$( "#map" ).append( '<img src="img/plains.png" style="width:64px;height:64px;"/>' );
@@ -26,15 +41,18 @@ $.getJSON( "map1/terrain", function( data ) {
   			{
   				$( "#map" ).append( '<img src="img/water.png" style="width:64px;height:64px;"/>' );
   			}
-  			
   			c++;
   		});
   		r++;
 	});
-
-
-
 });
+
+function moveUnit(unitId,x,y)
+{
+	
+}
+
+
 
 
 
